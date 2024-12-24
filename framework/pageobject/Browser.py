dbe1af52a5707dea5d04bd220d9a59f313e72872
Stepan Verevkin <verevkin@hotmail.com>
2024-12-25 00:19:03 +0300

    Fix 1.1

--- framework/pageobject/Browser.py
+++ framework/pageobject/Browser.py
@@ -11,12 +11,7 @@ logging.basicConfig(format='%(asctime)s - INFO - %(message)s', level=logging.INF
 
 class BrowserCls(metaclass=MetaSingleton):
     """
-    Performing initialization for WebDriver. A fixture is used in the description. 
-    Fixtures in pytest are functions that have their own frequency of execution.
-    With the help of a fixture, you can prepare the initial state of the system for testing.
-    We mark the function with its decorator @pytest.fixture and passing the scope parameter 
-    with the session value. This means that this fixture function will be executed 
-    only 1 time per test session.
+    Performing initialization for WebDriver and base methods for working with WebDriver.
     
     Methods:
     -----------
